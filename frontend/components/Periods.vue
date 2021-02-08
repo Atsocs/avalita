d2f642f187b3ce601312b42b06d69771eb5dd159
bissectra <caugcosta@gmail.com>
2021-02-08 17:10:44 -0300

    periods

--- frontend/components/Periods.vue
+++ frontend/components/Periods.vue
@@ -1,70 +1,43 @@
 <template>
-    <div>
-        {{ret}}
-        <v-expansion-panels>
-            <v-expansion-panel
-                    v-for="(item,i) in groupedItems(items)"
-                    :key="i"
-            >
-                <v-expansion-panel-header>
-                    {{item.period}}
-                </v-expansion-panel-header>
-                <v-expansion-panel-content v-for="(course, j) in item.courses">
-                    {{item.courses[j].professor.name}}<br>
-                    {{item.courses[j].course_abbr}}: {{item.courses[j].course_name}}
-                </v-expansion-panel-content>
-            </v-expansion-panel>
-        </v-expansion-panels>
-    </div>
+  <div>
+    <v-expansion-panels>
+      <v-expansion-panel
+        v-for="(item,name) in groupedItems(items)"
+        :key="name"
+      >
+        <v-expansion-panel-header>
+          {{name}}
+        </v-expansion-panel-header>
+        <v-expansion-panel-content v-for="(subItem, i) in item" :key="i"
+        class="text-center">
+          <!--          {{subItem}}<br>-->
+          <!--          {{subItem.professor.name}}-->
+          {{subItem.professor.name}}<br>
+          ({{subItem.course_abbr}} - {{subItem.course_name}})
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+    {{names}}
+  </div>
 </template>
 
 <script>
-    import _ from 'underscore'
+import _ from 'lodash'
 
-    export default {
-        name: 'Periods',
-        props: ['items'],
-        data () {
-            return {
-                ret: null
-            }
-        },
-        methods: {
-            groupedItems (x) {
-                //
-
-                const ret = _.mapValues(_.groupBy(x, 'period'),
-                    clist => clist.map(x => _.omit(x, 'period')))
-                this.ret = ret
-                return ret
-
-                // return [
-                //     {
-                //         period: '2020 - 1° semestre',
-                //         courses: [
-                //             {
-                //                 professor: {username: 'fpereira', name: 'Fulano Silva'},
-                //                 course_abbr: 'ART-01',
-                //                 course_name: 'Introdução à Carteação',
-                //                 rates: [0, 0, 0, 0]
-                //             }
-                //         ]
-                //     },
-                //     {
-                //         period: '2020 - 2° semestre',
-                //         courses: [
-                //             {
-                //                 professor: {username: 'fpereira', name: 'Beltrano Silva'},
-                //                 course_abbr: 'ART-02',
-                //                 course_name: 'Introdução à Carteação 2',
-                //                 rates: [0, 0, 0, 0]
-                //             }
-                //         ]
-                //     }
-                // ]
-            }
-        }
+export default {
+  name: 'Periods',
+  props: ['items'],
+  data () {
+    return {}
+  },
+  methods: {
+    groupedItems (x) {
+      //
+      return _.mapValues(_.groupBy(x, 'period'),
+        clist => clist.map(x => _.omit(x, 'period')))
     }
+  }
+}
 </script>
 
 <style scoped>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,33 +1,40 @@
 <template>
-    <periods :items="items" />
+  <periods :items="items" />
 </template>
 
 <script>
-    import Periods from '../components/Periods'
+import Periods from '../components/Periods'
 
-    export default {
-        components: {Periods},
-        data () {
-            return {
-                items: [
-                    {
-                        period: '2020 - 2° semestre',
-                        professor: {username: 'fpereira', name: 'Fulano Silva'},
-                        course_abbr: 'ART-01',
-                        course_name: 'Introdução à Carteação',
-                        rates: [0, 0, 0, 0]
-                    },
-                    {
-                        period: '2020 - 2° semestre',
-                        professor: {username: 'fpereira', name: 'Fulano Silva'},
-                        course_abbr: 'ART-01',
-                        course_name: 'Introdução à Carteação',
-                        rates: [0, 0, 0, 0]
-                    }
-                ]
-            }
+export default {
+  components: {Periods},
+  data () {
+    return {
+      items: [
+        {
+          period: '2020 - 1° semestre',
+          professor: {username: 'fsilva', name: 'Fulano Silva'},
+          course_abbr: 'ART01',
+          course_name: 'Introdução à Carteação',
+          rates: [0, 0, 0, 0]
+        },
+        {
+          period: '2020 - 2° semestre',
+          professor: {username: 'bsilva', name: 'Beltrano Silva'},
+          course_abbr: 'ART02',
+          course_name: 'Introdução à Carteação 2',
+          rates: [0, 0, 0, 0]
+        },
+        {
+          period: '2020 - 1° semestre',
+          professor: {username: 'csilva', name: 'Ciclano Silva'},
+          course_abbr: 'MAC01',
+          course_name: 'Introdução ao Macaco',
+          rates: [0, 0, 0, 0]
         }
+      ]
     }
+  }
+}
 </script>
 
 <style>
